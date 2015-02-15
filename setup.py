d0fbf6443c219117f838c5ec6eb3edd3a0fa61f9
Arnold Krille <arnold@arnoldarts.de>
2015-02-15 13:09:44 +0100

    start the coding dojo with some boiler-plate

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arnoldsfizzbuzzdojo',
+    author='Arnold Krille',
+    author_email='arnold@arnoldarts.de',
+    packages=find_packages()
+)
