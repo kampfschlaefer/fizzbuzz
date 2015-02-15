2418fd7e400939adcb926b45174efc9466451a80
Arnold Krille <arnold@arnoldarts.de>
2015-02-15 14:03:28 +0100

    less commented debug

--- fizzbuzz/no_if.py
+++ fizzbuzz/no_if.py
@@ -1,6 +1,5 @@
 
 def fizzbuzz_ternary(number):
-    # print(number, number % 3, not(number % 3), number % 5, not(number % 5))
     output = not(number % 3) and 'Fizz' or ''
     output += not(number % 5) and 'Buzz' or ''
     return output or str(number)
