94fa89098320275c70d5718dd6eb90f897e4c9f5
Arnold Krille <arnold@arnoldarts.de>
2015-02-15 13:51:07 +0100

    Improve the tests

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -9,18 +9,18 @@ algorithms = [basic_fizzbuzz]
 
 @pytest.mark.parametrize('algo', algorithms)
 def test_check_three(algo):
-    for i in range(3, 100, 3):
+    for i in range(0, 100, 3):
         assert 'Fizz' in algo(i)
 
 
 @pytest.mark.parametrize('algo', algorithms)
 def test_check_fives(algo):
-    for i in range(5, 100, 5):
+    for i in range(0, 100, 5):
         assert 'Buzz' in algo(i)
 
 
 @pytest.mark.parametrize('algo', algorithms)
 def test_check_numbers(algo):
-    for i in range(1, 100):
+    for i in range(0, 100):
         if i % 3 and i % 5:
-            assert int(algo(i)) == i
+            assert algo(i) == str(i)
